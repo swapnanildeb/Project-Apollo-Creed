4ef622efb1b3376c7c3473017accaf58617689dc
swapnanildeb <swapnanildeb@yahoo.com>
2015-09-05 14:43:41 -0400

    server

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+/*
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
-
+*/
 
 console.log("server starting");
 app.post('/',function(req,res){
